08f0b0a80a56786f8b76e67688bb080d3c752031
Saka289 <saka289@icloud.com>
2024-02-26 16:28:39 +0700

    feat: call api

---
+++ WebApp/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Dtos;
+using WebApp.Models;
+
+namespace WebApp.Service.IService
+{
+    public interface IBaseService
+    {
+        Task<ResponseDto?> SendAsync(ReestDto requestDto, bool withBearer = true);
+    }
+}
