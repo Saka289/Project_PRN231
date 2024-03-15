da58e823159c255ca084fb467048f667db11ada5
Saka289 <saka289@icloud.com>
2024-03-15 22:57:50 +0700

    upcode

---
+++ Web.Services.ShoppingCartAPI/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿using Web.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Web.Services.ShoppingCartAPI.Models
+{
+    public class Cart
+    {
+        public CartHeader CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+    }
+}
