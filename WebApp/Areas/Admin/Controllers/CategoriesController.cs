26857ce58dfe635b7f3ef059cf884154bc3e718f
Saka289 <saka289@icloud.com>
2024-03-19 22:09:22 +0700

    update

--- WebApp/Areas/Admin/Controllers/CategoriesController.cs
+++ WebApp/Areas/Admin/Controllers/CategoriesController.cs
@@ -9,24 +9,24 @@ namespace WebApp.Areas.Admin.Controllers
     [Area("Admin")]
     public class CategoriesController : Controller
     {
-        private readonly ICategoryService _categoryService;
-        public CategoriesController(ICategoryService categoryService)
-        {
-            _categoryService   = categoryService;
-        }
-        public async Task<IActionResult> Index()
-        {
-            List<CategoryDto> list = new List<CategoryDto>();
-            ResponseDto? response = await _categoryService.GetAllCategoryAsync();
-            if (response != null && response.IsSuccess)
-            {
-                list = JsonConvert.DeserializeObject<List<CategoryDto>>(Convert.ToString(response.Result));
-            }
-            else
-            {
-                TempData["error"] = response?.Message;
-            }
-            return View(list);
-        }
+        //private readonly ICategoryService _categoryService;
+        //public CategoriesController(ICategoryService categoryService)
+        //{
+        //    _categoryService   = categoryService;
+        //}
+        //public async Task<IActionResult> Index()
+        //{
+        //    List<CategoryDto> list = new List<CategoryDto>();
+        //    ResponseDto? response = await _categoryService.GetAllCategoryAsync();
+        //    if (response != null && response.IsSuccess)
+        //    {
+        //        list = JsonConvert.DeserializeObject<List<CategoryDto>>(Convert.ToString(response.Result));
+        //    }
+        //    else
+        //    {
+        //        TempData["error"] = response?.Message;
+        //    }
+        //    return View(list);
+        //}
     }
 }
